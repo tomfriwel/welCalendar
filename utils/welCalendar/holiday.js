01f5ab309075ae6ebd0ab0a19e0767bad4fd8561
tomfriwel <976932447@qq.com>
2018-09-13 16:07:53 +0800

    init

---
+++ utils/welCalendar/holiday.js
@@ -0,0 +1,23 @@
+const Holiday = {
+    // 年
+    2018: {
+        // 1~12月
+        9: {
+            // 1~31日
+            22: {
+                title: '中秋节',
+                range: [22, 24],
+                at: 24
+            }
+        },
+        10: {
+            1: {
+                title: '国庆节',
+                range: [1, 7],
+                at: 1
+            }
+        }
+    }
+}
+
+module.exports = Holiday
